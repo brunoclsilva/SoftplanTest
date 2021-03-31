ed9919ec2049b69295c1521fa34fe00b0503118e
BRUNO SILVA <bclsilva@sga.pucminas.br>
2021-03-31 17:39:05 -0300

    Add Api 2

--- SoftplanTest.Api2/Controllers/CalculaJurosController.cs
+++ SoftplanTest.Api2/Controllers/CalculaJurosController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SoftplanTest.Api2.Infrastructure.Interfaces;
+using System;
 
 namespace SoftplanTest.Api2.Controllers
 {
@@ -8,10 +10,51 @@ namespace SoftplanTest.Api2.Controllers
     public class CalculaJurosController : ControllerBase
     {
         private readonly ILogger<CalculaJurosController> _logger;
+        private readonly ICalculaJurosService _calculaJurosService;
 
-        public CalculaJurosController(ILogger<CalculaJurosController> logger)
+        public CalculaJurosController(ILogger<CalculaJurosController> logger,
+                                      ICalculaJurosService calculaJurosService)
         {
             _logger = logger;
+            _calculaJurosService = calculaJurosService;
+        }
+
+        [HttpGet]
+        [Route("/calculajuros")]
+        public IActionResult GetTaxaJuros(decimal valorInicial, int tempo)
+        {
+            try
+            {
+                _logger.LogInformation("Calculando taxa de juros...");
+                var response = _calculaJurosService.CalculaJurosAsync(valorInicial, tempo).Result;
+                _logger.LogInformation("Taxa de juros calculada!");
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error: {e.Message}");
+                throw new Exception($"Error: {e.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("/showmethecode")]
+        public IActionResult ShowMeTheCode()
+        {
+            try
+            {
+                _logger.LogInformation("Calculando taxa de juros...");
+                var response = "https://github.com/brunoclsilva/SoftplanTest/tree/master";
+                _logger.LogInformation("Taxa de juros calculada!");
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error: {e.Message}");
+                throw new Exception($"Error: {e.Message}");
+            }
         }
     }
 }
