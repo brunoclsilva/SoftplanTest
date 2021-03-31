19858c230683a03a44190a4bc87fcdd2926fc412
BRUNO SILVA <bclsilva@sga.pucminas.br>
2021-03-31 14:35:23 -0300

    Api 1 add

--- SoftplanTest.Api1/Controllers/TaxaJurosController.cs
+++ SoftplanTest.Api1/Controllers/TaxaJurosController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SoftplanTest.Api1.Infrastructure.Interfaces;
+using System;
 
 namespace SoftplanTest.Api1.Controllers
 {
@@ -8,10 +10,32 @@ namespace SoftplanTest.Api1.Controllers
     public class TaxaJurosController : ControllerBase
     {
         private readonly ILogger<TaxaJurosController> _logger;
+        private readonly ITaxaJurosService _taxaJurosService;
 
-        public TaxaJurosController(ILogger<TaxaJurosController> logger)
+        public TaxaJurosController(ILogger<TaxaJurosController> logger,
+                                   ITaxaJurosService taxaJurosService)
         {
             _logger = logger;
+            _taxaJurosService = taxaJurosService;
+        }
+
+        [HttpGet]
+        [Route("/taxaJuros")]
+        public IActionResult GetTaxaJuros()
+        {
+            try
+            {
+                _logger.LogInformation("Buscando taxa de juros...");
+                var response = _taxaJurosService.GetTaxaJuros();
+                _logger.LogInformation("Taxa de juros encontrada!");
+
+                return Ok(response.Result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error: {e.Message}");
+                throw new Exception($"Error: {e.Message}");
+            }
         }
     }
 }
