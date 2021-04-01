3af0eda83a05d01e3cb773be1e82c23d17d9bc62
BRUNO SILVA <bclsilva@sga.pucminas.br>
2021-03-31 22:00:33 -0300

    Add testes

--- SoftplanTest.Api1.Test/Testes.cs
+++ SoftplanTest.Api1.Test/Testes.cs
@@ -1,4 +1,6 @@
-using System;
+using SoftplanTest.Api1.Infrastructure.Repositories;
+using SoftplanTest.Api1.Infrastructure.Services;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SoftplanTest.Api1.Test
@@ -6,9 +8,21 @@ namespace SoftplanTest.Api1.Test
     public class Testes
     {
         [Fact]
-        public void Test1()
+        public void GetTaxaJuros_RetornaDecimal()
         {
+            var taxaJurosService = new TaxaJurosService(new TaxaJurosRepository());
 
+            Assert.IsType<Task<decimal>>(taxaJurosService.GetTaxaJuros());
+        }
+
+        [Fact]
+        public void GetTaxaJuros_E_Igual_1Porcento()
+        {
+            var taxaJurosService = new TaxaJurosService(new TaxaJurosRepository());
+
+            decimal valor = 0.01m;
+
+            Assert.Equal(taxaJurosService.GetTaxaJuros().Result, valor);
         }
     }
 }
--- SoftplanTest.Api2.Test/Testes.cs
+++ SoftplanTest.Api2.Test/Testes.cs
@@ -1,4 +1,9 @@
+using NSubstitute;
+using SoftplanTest.Api2.Infrastructure.Services;
 using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SoftplanTest.Api2.Test
@@ -6,9 +11,42 @@ namespace SoftplanTest.Api2.Test
     public class Testes
     {
         [Fact]
-        public void Test1()
+        public void CalculaJuros_Retorna_Decimal()
         {
+            var httpClientFactoryMock = Substitute.For<IHttpClientFactory>();
 
+            var calculaJurosService = new CalculaJurosService(httpClientFactoryMock);
+
+            var valorInicial = 100.00m;
+            var tempo = 5;
+
+            Assert.IsType<Task<decimal>>(calculaJurosService.CalculaJurosAsync(valorInicial, tempo));
+        }
+
+        [Fact]
+        public void ValorInicial_Menor_Que_Zero()
+        {
+            var httpClientFactoryMock = Substitute.For<IHttpClientFactory>();
+
+            var calculaJurosService = new CalculaJurosService(httpClientFactoryMock);
+
+            var valorInicial = -100.00m;
+            var tempo = 5;
+
+            Assert.ThrowsAsync<Exception>(() => calculaJurosService.CalculaJurosAsync(valorInicial, tempo));
+        }
+
+        [Fact]
+        public void Tempo_Menor_Que_Zero()
+        {
+            var httpClientFactoryMock = Substitute.For<IHttpClientFactory>();
+
+            var calculaJurosService = new CalculaJurosService(httpClientFactoryMock);
+
+            var valorInicial = 100.00m;
+            var tempo = -5;
+
+            Assert.ThrowsAsync<Exception>(() => calculaJurosService.CalculaJurosAsync(valorInicial, tempo));
         }
     }
 }
